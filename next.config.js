894c0d26708d225b91514983e24351bc73e8df8d
Daniel <Daniel-feili@hotmail.no>
2025-03-07 00:48:03 +0100

    test3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { truncate } = require('node:fs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
+    basePath: "https://danofe.github.io/",
     assetPrefix: "/public/images/",
     output: "export",  // <=== enables static exports
     reactStrictMode: false,
